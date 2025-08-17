30e636485cf2ea42789c30241703aa1b2711d656
りんたま <rintyan2951@gmail.com>
2025-08-18 07:28:51 +0900

    shiftjsをutf8にする

--- beat-striker/Assets/Scripts/EnemyController.cs
+++ beat-striker/Assets/Scripts/EnemyController.cs
@@ -6,7 +6,7 @@ public class EnemyController : MonoBehaviour
 
     void Start()
     {
-        // ƒvƒŒƒCƒ„[‚ğŒ©‚Â‚¯‚é
+        // ãƒ—ãƒ¬ã‚¤ãƒ¤ãƒ¼ã‚ªãƒ–ã‚¸ã‚§ã‚¯ãƒˆã‚’ã‚·ãƒ¼ãƒ³ã‹ã‚‰æ¢ã™
         player = GameObject.Find("Player");
     }
 
