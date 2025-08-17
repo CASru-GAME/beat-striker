865379a551f082bd8716acf02d01cd30112ec491
りんたま <rintyan2951@gmail.com>
2025-08-18 06:47:55 +0900

    チュートリアル

---
+++ beat-striker/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private GameObject player;
+
+    void Start()
+    {
+        //ƒvƒŒƒCƒ„[‚ğæ“¾
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            player.transform.position,
+            Time.deltaTime * 2.0f
+        );
+    }
+}
