8c8186bb9413812427cc0e86782f0974b40a9495
irukaman <a22160910@gmail.com>
2025-11-01 18:05:01 +0900

    勇者

--- beat-striker/Assets/HeroAttackSMB.cs
+++ beat-striker/Assets/HeroAttackSMB.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class HeroAttackSMB : StateMachineBehaviour {
     Colliden colliden;
+    [SerializeField] private GameObject hitEffectPrefab; // ヒットエフェクトのプレハブ
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         colliden = animator.GetComponent<StrikerView>().GetColliden("sword");
         colliden.OnEnterTrigger += OnEnterTrigger;
@@ -17,6 +18,12 @@ public class HeroAttackSMB : StateMachineBehaviour {
         var target = other.GetComponent<StrikerView>();
         if (target != null) {
             target.TakeDamage(new HitStatus(new HitPoint(10)));
+            
+            // ヒットエフェクトの生成
+            if (hitEffectPrefab != null) {
+                Vector3 hitPosition = other.ClosestPoint(colliden.transform.position);
+                Instantiate(hitEffectPrefab, hitPosition, Quaternion.identity);
+            }
         }
     }
     }
