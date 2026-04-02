c65ad0eb31ba899702788fa69c15b2b2fa21233e
りんたま <87462604+rintify@users.noreply.github.com>
2026-04-02 18:59:07 +0900

    turnをみんなに配る

---
+++ beat-striker/Assets/Project/Strikers/LargeWizard/States/TurnState.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using Alice;
+
+namespace Core.LargeWizard {
+
+    public class TurnState : StrikerState {
+        [SerializeField] private StrikerAnimationClip animationClip;
+        [SerializeField] StrikerNode nextNode;
+        [SerializeField] float duration = 0.2f;
+
+        const float COMPLETION_ANGLE_EPSILON = 0.01f;
+
+        Quaternion targetRotation;
+        RigidbodyConstraints originalConstraints;
+        bool turnCompleted;
+
+        public override void OnEnter(IStrikerContext context) {
+            originalConstraints = context.Rigidbody.constraints;
+            context.Rigidbody.constraints = originalConstraints & ~RigidbodyConstraints.FreezeRotation;
+
+            var startRotation = context.Rigidbody.rotation;
+            targetRotation = startRotation * Quaternion.Euler(0f, 180f, 0f);
+            turnCompleted = false;
+            context.PlayAnimation(animationClip);
+        }
+
+        public override void OnUpdate(IStrikerStateContext context) {
+            if (turnCompleted) {
+                return;
+            }
+
+            var turnSpeed = duration <= 0f ? 3600f : 180f / duration;
+            var nextRotation = Quaternion.RotateTowards(context.Rigidbody.rotation, targetRotation, turnSpeed * Time.deltaTime);
+            context.Rigidbody.MoveRotation(nextRotation);
+
+            if (Quaternion.Angle(nextRotation, targetRotation) > COMPLETION_ANGLE_EPSILON) {
+                return;
+            }
+
+            context.Rigidbody.MoveRotation(targetRotation);
+            turnCompleted = true;
+            context.TryTransition(nextNode);
+        }
+
+        public override void OnExit(IStrikerContext context) {
+            context.Rigidbody.constraints = originalConstraints;
+        }
+    }
+}
---
+++ beat-striker/Assets/Project/Strikers/LargeWizard/States/TurnableGroup.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Alice;
+
+namespace Core.LargeWizard {
+    
+    public class TurnableGroup : StrikerGroup {
+        [SerializeField] StrikerNode turnState;
+
+        // このグループに入った時に呼ばれる（前のステートがこのグループに所属していなかった場合）
+        public override void OnEnter(IStrikerContext context) {
+        }
+
+        // このグループから出る時に呼ばれる（次のステートがこのグループに所属していない場合）
+        public override void OnExit(IStrikerContext context) {
+        }
+
+        public override void OnEnemyBehind(IStrikerStateContext context) {
+            context.TryTransition(turnState, true);
+        }
+
+    }
+}
