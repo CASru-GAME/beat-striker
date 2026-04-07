722a58796776f87af5dc4e1c6ffe37e4be5bd766
irukaman <a22160910@gmail.com>
2026-04-07 14:17:22 +0900

    a

--- beat-striker/Assets/Project/Strikers/LargeHero/states/SpecialFallState.cs
+++ beat-striker/Assets/Project/Strikers/LargeHero/states/SpecialFallState.cs
@@ -9,10 +9,12 @@ namespace Core.LargeHero {
         [SerializeField] private GroundChecker groundChecker;
         [SerializeField] private StrikerNode landNode;
         [SerializeField] private SpecialSequenceContext specialSequenceContext;
+        [SerializeField] private ParticleSystem slashEffect;
         [SerializeField] private float fallSpeed = 15f;
 
         public override void OnEnter(IStrikerContext context) {
             context.PlayAnimation(animationClip);
+            slashEffect.Play();
         }
 
         public override void OnUpdate(IStrikerStateContext context) {
--- beat-striker/Assets/Project/Strikers/LargeHero/states/SpecialLandState.cs
+++ beat-striker/Assets/Project/Strikers/LargeHero/states/SpecialLandState.cs
@@ -8,11 +8,14 @@ namespace Core.LargeHero {
         [SerializeField] private StrikerAnimationClip animationClip;
         [SerializeField] private StrikerNode nextNode;
         [SerializeField] private SpecialSequenceContext specialSequenceContext;
+        [SerializeField] private ParticleSystem slashEffect;
         [SerializeField] private float finalDamage = 20f;
         [SerializeField] private float finalKnockbackSpeedX = 12f;
         [SerializeField] private float finalKnockbackSpeedY = 4f;
 
         public override void OnEnter(IStrikerContext context) {
+            slashEffect.Play();
+
             specialSequenceContext.ReleaseVictimWithFinalHit(context.Rigidbody, finalDamage, finalKnockbackSpeedX, finalKnockbackSpeedY);
             context.PlayAnimation(animationClip, OnAnimationEnd);
         }
