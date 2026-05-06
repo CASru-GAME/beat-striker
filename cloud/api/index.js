d11e912fc221a59c863ebbc9b766498ca8d9f825
りんたま <87462604+rintify@users.noreply.github.com>
2026-05-07 05:36:40 +0900

    cloud

--- cloud/api/index.js
+++ cloud/api/index.js
@@ -3,6 +3,15 @@ import { Firestore } from "@google-cloud/firestore";
 
 const app = express();
 const db = new Firestore();
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
 
 app.use(express.json({ limit: "25mb" }));
 
